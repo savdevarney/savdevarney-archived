7e964eb983088daba2b6139f36679128733e1983
sdevarney <sdevarney@hq.bill.com>
2018-05-29 16:08:41 -0700

    nav bar with basic routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { WritingComponent } from './writing/writing.component';
+import { ProjectsComponent } from './projects/projects.component';
 
+const appRoutes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'writings', component: WritingComponent },
+  { path: 'projects', component: ProjectsComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    AboutComponent,
+    WritingComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
